c17ef19cddcac37897da207b4645e5739004d459
AliKhan <baybutov95@mail.ru>
2025-02-20 03:09:45 +0500

    make city data request

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+
+type CityData = {
+  country: "GB";
+  lat: number;
+  local_names: object;
+  lon: number;
+  name: string;
+  state: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [city, setCity] = useState<CityData[]>();
+
+  const getCityURL = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=${
+    import.meta.env.VITE_API_KEY
+  }`;
+
+  const getCity = useCallback(async () => {
+    const { data } = await axios.get(getCityURL);
+
+    console.log(data);
+
+    setCity(data);
+  }, [getCityURL]);
+
+  useEffect(() => {
+    getCity();
+  }, [getCity]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="text-gray-600">{city && city[0].name}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
