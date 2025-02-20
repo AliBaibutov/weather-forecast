c0899c14270ae56e950fe1d90c88a7c3edb1e119
AliKhan <baybutov95@mail.ru>
2025-02-20 17:09:59 +0500

    make weather request

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,23 @@ type CityData = {
   state: string;
 };
 
+type WeatherData = {
+  cod: string;
+};
+
 function App() {
   const [city, setCity] = useState<CityData[]>();
+  const [weatherForecast, setWeatherForecast] = useState<WeatherData>();
+  const [lat, setLat] = useState();
+  const [lon, setLon] = useState();
+
+  console.log(weatherForecast);
 
   const getCityURL = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=${
+    import.meta.env.VITE_API_KEY
+  }`;
+
+  const getWeatherForecastURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${
     import.meta.env.VITE_API_KEY
   }`;
 
@@ -23,15 +36,29 @@ function App() {
     console.log(data);
 
     setCity(data);
+    setLat(data[0].lat);
+    setLon(data[0].lon);
   }, [getCityURL]);
+
+  const getWeather = useCallback(async () => {
+    const { data } = await axios.get(getWeatherForecastURL);
+
+    console.log(data);
+
+    setWeatherForecast(data);
+  }, [getWeatherForecastURL]);
 
   useEffect(() => {
     getCity();
-  }, [getCity]);
+    if (lat && lon) getWeather();
+  }, [getCity, getWeather, lat, lon]);
 
   return (
     <>
       <div className="text-gray-600">{city && city[0].name}</div>
+      <div className="text-gray-600">
+        {weatherForecast && weatherForecast.cod}
+      </div>
     </>
   );
 }
